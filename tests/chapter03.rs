f8e224606e6516985c55b99627368ee487e17717
li ou <3212728662@qq.com>
2022-07-05 10:49:05 +0800

    update

--- tests/chapter03.rs
+++ tests/chapter03.rs
@@ -2,6 +2,7 @@
 #[cfg(test)]
 mod test{
     use std::collections::HashMap;
+    use serde::de::Unexpected::Option;
     use tracing_subscriber::fmt::try_init;
 
     // Box智能指针
@@ -49,6 +50,11 @@ mod test{
         // let mut hash_map = HashMap::<Box<dyn IHashMap>,Box<dyn IHashMap2>>::new();
         // hash_map.insert(Box::new(Go{}),Box::new(Rust{}));
         // assert_eq!(hash_map.len(),1);
+
+        // let mut hash_map = HashMap::<T,U>::new();
+        // hash_map.insert("aa",11);
+        // hash_map.insert(22,"bb");
+        // assert_eq!(2,hash_map.len());//Error
     }
 
     // 移动语义
@@ -88,13 +94,71 @@ mod test{
         println!("{:?}",outer_val);
         //println!("{:?}",inner_val);//Error, inner_val 不在当前作用域内 cannot find value `inner_val` in this scope
         //println!("{:?}",outer_sp); //Error, borrow of moved value: `outer_sp`
-
-        // match 匹配中的词法作用域
-        let a = "hello".to_string();
-        match a {
-            Some(s) => println!("{:?}",s),
-            _ => println!("nothing"),
-        }
-        println!("a = {:?}",a);
     }
+
+    // match 匹配词法作用域
+    #[test]
+    fn test_match_lifetime_scope(){
+        // match 匹配中的词法作用域
+        // let a = Some("hello".to_string());
+        // match a {
+        //     Some(s) => println!("{:?}",s),
+        //     _ => println!("nothing"),
+        // }
+        // println!("a = {:?}",a);//Error
+    }
+
+    // Vector 词法作用域
+    #[test]
+    fn test_vec_lifetime_scope(){
+        let v = vec![1,2,3];
+        for i in v {
+            println!("{:?}",i);
+            //println!("{:?}",v); Error value borrowed here after move
+        }
+        let vec = vec![4,5,6];
+        for i in vec.iter().enumerate(){
+            println!("index = {:?}",i.0);
+            println!("value = {:?}",i.1);
+        }
+    }
+
+    // if let 判断的词法作用域
+    #[test]
+    fn test_if_let_lifetime(){
+        let a = Some("hello world".to_string());
+        if let Some(s) = a {// 此时 a 的所有权被转移到这个表达式这里
+            println!("{:?}",s);
+            //println!("{:?}",a);//Error value borrowed here after partial move
+        }
+    }
+
+    // 函数的词法作用域
+    // 函数参数 s String 类型，是 move 语义，因此 s 的所有权被转移到了函数内部
+    fn foo(s: String) -> String {
+        let w = "world".to_string();
+        s + &w
+    }
+    #[test]
+    fn test_fn_lifetime_scope(){
+        let s = "hello".to_string();
+        let ss = foo(s);
+        println!("{:?}",ss);
+        //println!("{:?}",s); //Error value borrowed here after move
+    }
+
+    #[test]
+    fn test_closure(){
+        let mut optional = Some(0); // optional 为 Option<i32> 类型，为复制语义，
+        while let Some(i) = optional{ // 判断 optional 的值，while let 后，变量i的所有权未转移
+            if i > 9 {
+                println!("Greater than 9,quit");
+                optional = None;
+            }else {
+                println!("`i` is `{:?}`.Try again.",i);
+                optional = Some(i+1);
+            }
+        }
+    }
+
 }
